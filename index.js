16925da14385f4f82851d8176dc760ad3eb74798
simonare <simonarez@hotmail.com>
2024-06-28 16:41:55 +0300

    initial commit

--- index.js
+++ index.js
@@ -2,19 +2,47 @@ process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}`;
 const wkhtmltopdf = require("./utils/wkhtmltopdf");
 const errorUtil = require("./utils/error");
 
-exports.handler = function handler(event, context, callback) {
-    if (!event.html) {
-        const errorResponse = errorUtil.createErrorResponse(400, "Validation error: Missing field 'html'.");
-        callback(errorResponse);
-        return;
+exports.handler = async (event, context) => {
+  try {
+    const body = JSON.parse(event.body);
+    if (!body?.html) {
+      const errorResponse = errorUtil.createErrorResponse(
+        400,
+        "Validation error: Missing field 'html'."
+      );
+      return {
+        statusCode: 400,
+        body: JSON.stringify(errorResponse),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
     }
 
-    wkhtmltopdf(event.html)
-        .then(buffer => {
-            callback(null, {
-                data: buffer.toString("base64")
-            });
-        }).catch(error => {
-            callback(errorUtil.createErrorResponse(500, "Internal server error", error));
-        });
+    const filename = body.filename || "document.pdf";
+
+    const buffer = await wkhtmltopdf(body.html, body.options);
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename=${filename}`,
+      },
+      body: buffer.toString("base64"),
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    const errorResponse = errorUtil.createErrorResponse(
+      500,
+      "Internal server error",
+      error
+    );
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errorResponse),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
 };
--- utils/wkhtmltopdf.js
+++ utils/wkhtmltopdf.js
@@ -1,26 +1,40 @@
 const { spawn } = require("child_process");
 
 module.exports = function (html, options = []) {
-    return new Promise(((resolve, reject) => {
-        const bufs = [];
-        const proc = spawn("/bin/sh", ["-o", "pipefail", "-c", `LD_LIBRARY_PATH=lib lib/wkhtmltopdf ${options.join(" ")} - - | cat`]);
+  return new Promise((resolve, reject) => {
+    const bufs = [];
+    const proc = spawn("/bin/sh", [
+      "-o",
+      "pipefail",
+      "-c",
+      `LD_LIBRARY_PATH=lib lib/wkhtmltopdf ${options.join(" ")} - - | cat`,
+    ]);
 
-        proc.on("error", error => {
-            reject(error);
-        }).on("exit", code => {
-            if (code) {
-                reject(new Error(`wkhtmltopdf process exited with code ${code}`));
-            } else {
-                resolve(Buffer.concat(bufs));
-            }
-        });
+    proc
+      .on("error", (error) => {
+        reject(error);
+      })
+      .on("exit", (code) => {
+        if (code) {
+          reject(new Error(`wkhtmltopdf process exited with code ${code}`));
+        } else {
+          resolve(Buffer.concat(bufs));
+        }
+      });
 
-        proc.stdin.end(html);
+    proc.stdin.write(html);
+    proc.stdin.end();
 
-        proc.stdout.on("data", data => {
-            bufs.push(data);
-        }).on("error", error => {
-            reject(error);
-        });
-    }));
+    proc.stdout
+      .on("data", (data) => {
+        bufs.push(data);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+
+    proc.stderr.on("data", (data) => {
+      console.error(`wkhtmltopdf stderr: ${data}`);
+    });
+  });
 };
